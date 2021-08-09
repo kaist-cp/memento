bb252cd11ec913a3749de1250e1c15972e039737
Seungmin Jeon <seungmin.jeon@cp.kaist.ac.kr>
2021-08-09 11:50:08 +0900

    ploc with client: 주석 정리

--- src/plocation/pool.rs
+++ src/plocation/pool.rs
@@ -12,27 +12,24 @@ use super::global::{self, global_pool};
 use super::ptr::PersistentPtr;
 use memmap::*;
 
-// TODO: client 인터페이스에 맞게 doc test 수정
 /// 열린 풀을 관리하기 위한 풀 핸들러
 ///
-/// # 풀을 열고 사용하는 예시
+/// # 핸들러 사용예시
 ///
 /// ```
-/// use std::fs::remove_file;
-/// use compositional_persistent_object::plocation::pool::Pool;
-///
-/// // (1) 기존의 파일은 제거하고 풀 파일 새로 생성
-/// let _ = remove_file("foo.pool");
-/// let _ = Pool::create::<i32>("foo.pool", 8 * 1024);
-///
-/// // (2) 풀 열기
-/// let pool_handle = Pool::open("foo.pool").unwrap();
-///
-/// // (3) 루트 오브젝트를 가져와서 사용
-/// let mut head = pool_handle.get_root::<i32>().unwrap();
-/// let mut head = unsafe { head.deref_mut() };
-/// *head = 5;
-/// assert_eq!(*head, 5);
+/// // TODO: client 인터페이스에 맞게 doc test 수정
+/// //
+/// // use compositional_persistent_object::plocation::pool::Pool;
+/// //
+/// // // 풀 생성 후 열어서 풀 핸들러 얻기
+/// // let _ = Pool::create::<i32>("foo.pool", 8 * 1024);
+/// // let pool_handle = Pool::open("foo.pool").unwrap();
+/// //
+/// // // 핸들러로부터 루트 오브젝트를 가져와서 사용
+/// // let mut head = pool_handle.get_root::<i32>().unwrap();
+/// // let mut head = unsafe { head.deref_mut() };
+/// // *head = 5;
+/// // assert_eq!(*head, 5);
 /// ```
 #[derive(Debug)]
 pub struct PoolHandle {
@@ -56,7 +53,7 @@ impl PoolHandle {
         self.start() + self.len
     }
 
-    /// 풀의 루트 오브젝트를 가리키는 포인터 반환
+    /// 풀의 루트(루트 오브젝트/루트 클라이언트 tuple)를 가리키는 포인터 반환
     #[inline]
     pub fn get_root<O: PersistentOp<C>, C: PersistentClient>(
         &self,
@@ -83,7 +80,7 @@ impl PoolHandle {
     }
 }
 
-/// 풀의 내부 관리 및 풀을 열고/닫기 위한 역할
+/// 풀 열기/닫기 및 메타데이터를 관리하는 역할
 ///
 /// # Pool Address Layout
 ///
@@ -94,14 +91,14 @@ pub struct Pool {
     /// 풀의 시작주소로부터 루트 오브젝트/클라이언트까지의 거리
     root_offset: usize,
 
-    /// 풀의 메타데이터 및 루트 오브젝트/클라이언트까지 잘 초기화되었는지 여부
-    // TODO: 초기값=false 보장가능한지 알아보기 (파일 생성시 0으로 초기화됨을 보장가능한가?)
+    /// 풀이 잘 초기화 되었는지 여부
+    // TODO: 파일 생성시 초기값=false 보장가능한지 알아보기
     is_initialized: bool,
     // TODO: 풀의 메타데이터는 여기에 필드로 추가
 }
 
 impl Pool {
-    /// 풀 내부 초기화 (메타데이터, 루트 오브젝트/클라이언트)
+    /// 풀 내부 초기화 (메타데이터, 루트 오브젝트/클라이언트 초기화)
     fn init<O: Default + PersistentOp<C>, C: PersistentClient>(&mut self, start: usize) {
         // e.g. 메타데이터 크기(size_of::<Pool>)가 16이라면, 루트는 풀의 시작주소+16에 위치
         self.root_offset = mem::size_of::<Pool>();
@@ -115,7 +112,7 @@ impl Pool {
         self.is_initialized = true;
     }
 
-    /// 풀 생성: 풀로서 사용할 파일을 생성하고 풀 레이아웃에 맞게 파일의 내부구조 초기화
+    /// 풀 생성 (풀로서 사용할 파일을 생성하고 풀 레이아웃에 맞게 파일의 내부구조 초기화)
     pub fn create<O: Default + PersistentOp<C>, C: PersistentClient>(
         filepath: &str,
         size: usize,
@@ -139,7 +136,7 @@ impl Pool {
         Ok(())
     }
 
-    /// 풀 열기: 파일을 persistent heap으로 매핑 후 풀 핸들러 반환
+    /// 풀 열기 (파일을 persistent heap으로 매핑 후 풀 핸들러 반환)
     pub fn open(filepath: &str) -> Result<&PoolHandle, Error> {
         // 1. 파일 열기 (파일이 존재하지 않는다면 실패)
         let file = OpenOptions::new().read(true).write(true).open(filepath)?;
@@ -185,12 +182,12 @@ impl Pool {
 
 #[cfg(test)]
 mod test {
+    use env_logger as _;
+    use log::{self as _, debug};
     use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering::SeqCst};
 
     use crate::persistent::PersistentOp;
     use crate::plocation::pool::*;
-    use env_logger as _;
-    use log::{self as _, debug};
 
     struct RootObj {
         value: AtomicUsize,
@@ -231,7 +228,8 @@ mod test {
     }
 
     struct RootClient {
-        // e.g. 만약 RootObj에 Queue가 들어간다면 여기에 Queue를 위한 Push/PopClient를 필드로 추가
+        // 이 테스트는 간단한 예제이기 때문에 `RootClient` 필드가 비어있음
+    // 그러나 만약 `RootObj`에 Queue가 들어간다면 Queue를 위한 Push/PopClient를 필드로 추가해야함
     }
 
     impl Default for RootClient {
@@ -258,128 +256,13 @@ mod test {
         // 풀 없으면 새로 만듦
         let _ = Pool::create::<RootObj, RootClient>(FILE_NAME, FILE_SIZE).is_ok();
 
-        // 풀 열기
+        // 풀 열고 루트 오브젝트, 루트 클라이언트 가져오기
         let pool_handle = Pool::open(FILE_NAME).unwrap();
         let mut root_ptr = pool_handle.get_root::<RootObj, RootClient>().unwrap();
         let (root_obj, root_client) = unsafe { root_ptr.deref_mut() };
 
-        // flag=1 => value=42를 보장하는 persistent op의 entry point
+        // 루트 클라이언트로 루트 오브젝트의 op 실행
+        // 이 경우 루트 오브젝트의 op은 invariant 검사하는 `check_inv()`
         root_obj.persistent_op(root_client, ()).unwrap();
     }
-}
-
-#[cfg(test)]
-mod test_node {
-    // use crate::plocation::pool::*;
-    // use env_logger as _;
-    // use log as _;
-    // use std::fs::remove_file;
-
-    // struct Node {
-    //     value: usize,
-    //     next: PersistentPtr<Node>,
-    // }
-
-    // impl Node {
-    //     fn new(value: usize) -> Self {
-    //         Self {
-    //             value,
-    //             next: PersistentPtr::null(),
-    //         }
-    //     }
-    // }
-
-    // /// persistent pool에 노드를 할당하고 다시 열었을 때 매핑된 주소가 바뀌어도 할당되었던 노드를 잘 따라가는지를 확인
-    // /// idempotency 테스트는 아님: persistent location이 잘 동작하는 지 확인하기 위한 테스트
-    // #[test]
-    // fn append_one_node() {
-    //     const FILE_NAME: &str = "test/append_one_node.pool";
-    //     const FILE_SIZE: usize = 8 * 1024;
-
-    //     // 루트 오브젝트로 Node를 가진 8MB 크기의 풀 파일 새로 생성
-    //     let _ = remove_file(FILE_NAME);
-    //     let _ = Pool::create::<Node>(FILE_NAME, FILE_SIZE).unwrap();
-
-    //     // 첫 번째 open: 노드 할당 후 루트 오브젝트에 연결
-    //     let mapped_addr1 = {
-    //         let pool_handle = Pool::open(FILE_NAME).unwrap();
-    //         let mapped_addr1 = pool_handle.start();
-
-    //         // 첫 번째 open이므로 루트 오브젝트부터 초기화
-    //         let mut head = pool_handle.get_root::<Node>().unwrap();
-    //         let head = unsafe { head.deref_mut() };
-    //         *head = Node::new(0);
-
-    //         // 풀에 새로운 노드 할당, 루트 오브젝트에 연결
-    //         // 결과: head(val: 0) -> node1(val: 1) -> ㅗ
-    //         if head.next.is_null() {
-    //             let mut node1 = pool_handle.alloc::<Node>();
-    //             unsafe {
-    //                 *node1.deref_mut() = Node::new(1);
-    //             }
-    //             // TODO: 여기서 터지면 node1은 leak됨. allocator 구현 후 이러한 leak도 없게하기
-    //             head.next = node1;
-    //         }
-
-    //         // NOTE
-    //         // - 여기서 풀을 닫지 않아야 두 번째 open할 때 다른 주소에 매핑됨
-    //         // - 풀을 닫으면 같은 파일에 대해선 같은 주소로 매핑
-    //         mapped_addr1
-    //     };
-
-    //     // 두 번째 open: 첫 번째에서 구성한 풀이 다른 주소로 매핑되어도 노드를 잘 따라가는지 확인
-    //     {
-    //         let pool_handle = Pool::open(FILE_NAME).unwrap();
-    //         let mapped_addr2 = pool_handle.start();
-    //         // 첫 번째 open의 매핑 정보가 drop되기 전에 두 번째 open을 하므로, 다른 주소에 매핑됨을 보장
-    //         assert_ne!(mapped_addr1, mapped_addr2);
-
-    //         // 첫 번째 open에서 구성한 풀대로 노드를 잘 따라가는지 확인
-    //         let head = pool_handle.get_root::<Node>().unwrap();
-    //         let head = unsafe { head.deref() };
-    //         let node1 = unsafe { head.next.deref() };
-    //         // 확인하기: head(val: 0) -> node1(val: 1) -> ㅗ
-    //         assert_eq!(head.value, 0);
-    //         assert_eq!(node1.value, 1);
-    //         assert!(node1.next.is_null());
-
-    //         Pool::close();
-    //     };
-    // }
-
-    // TODO: allocator 구현 후 테스트
-    // #[test]
-    // fn append_n_node() {
-    //     const N: usize = 100;
-
-    //     // 첫 번째 open: pm pool로 사용할 파일을 새로 만들고 그 안에 N개의 노드를 넣음
-    //     let _ = remove_file("append_n_node.pool");
-    //     {
-    //         let mut head = Pool::open::<Node>("append_n_node.pool").unwrap();
-
-    //         // N개의 노드 넣기
-    //         let mut p = head.deref_mut();
-    //         for i in 0..N {
-    //             let mut node = PPtr::<Node>::new();
-    //             node.value = i + 1; // 각 노드의 값은 자신이 몇 번째 노드인지랑 같음
-    //             p.next = node;
-    //             p = p.next.deref_mut();
-    //         }
-    //     }
-
-    //     // 두 번째 open: 첫 번째에서 구성한 pool이 다른 주소로 매핑되어도 노드를 잘 따라가는지 확인
-    //     {
-    //         let head: PPtr<Node> = Pool::open::<Node>("append_n_node.pool").unwrap();
-
-    //         // N-1번째 노드까지 따라가면서 첫 번째 open에서 구성한 대로 되어있는지 확인
-    //         let mut p = head.deref();
-    //         for i in 0..N {
-    //             assert_eq!(p.value, i);
-    //             p = p.next.deref();
-    //         }
-    //         // N번째 노드 확인
-    //         assert_eq!(p.value, N);
-    //         assert!(p.next.is_null());
-    //     }
-    // }
 }
