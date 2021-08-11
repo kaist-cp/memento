46c4bf184982a39b7078e65cbb5f0630a7fca759
Seungmin Jeon <seungmin.jeon@cp.kaist.ac.kr>
2021-08-11 11:39:10 +0900

    util: fix comment

--- src/util.rs
+++ src/util.rs
@@ -4,7 +4,7 @@ use std::ffi::OsString;
 
 /// 테스트 파일이 위치할 경로 계산
 ///
-/// e.g. "foo.pool" => "/test/foo.pool"
+/// e.g. "foo.pool" => "{project-path}/test/foo.pool"
 pub fn get_test_path(filename: &str) -> OsString {
     let mut path = std::path::PathBuf::new();
     path.push(env!("CARGO_MANIFEST_DIR")); // 프로젝트 경로
