ff4f03bf86361fbc0137c4e50a2b7452f6485836
Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>
2021-10-20 17:20:00 +0900

    eval: Set base dir
    Signed-off-by: Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>

--- evaluation/build.sh
+++ evaluation/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+base_dir=$(dirname "$0")
 dir_path=$(dirname $(realpath $0))
 target_path=$dir_path/target/release
 mkdir -p $target_path
@@ -27,4 +28,4 @@ fi
 # Build C++ implementation
 ### TODO: Corundum은 PMDK 컴파일시 왜 -O2 사용?
 ### TODO: PMDK도 no_persist 같은 옵션 있는지 확인하고 있다면 적용 (참고: https://pmem.io/2015/06/12/pmem-model.html)
-g++ -O3 -o $target_path/bench_cpp src/main.cpp src/pmdk/pipe.cpp -pthread -lpmemobj -std=c++17
+g++ -O3 -o $target_path/bench_cpp $base_dir/src/main.cpp $base_dir/src/pmdk/pipe.cpp -pthread -lpmemobj -std=c++17
