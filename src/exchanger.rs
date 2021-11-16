dd2493d524ec1013288b152332fd4fd95f730724
Seungmin Jeon <seungmin.jeon@cp.kaist.ac.kr>
2021-11-16 18:38:36 +0900

    Elim stack에 GC 적용

--- src/exchanger.rs
+++ src/exchanger.rs
@@ -758,7 +758,6 @@ mod tests {
 
     /// 스레드 여러 개의 exchange
     // TODO: #[serial] 대신 https://crates.io/crates/rusty-fork 사용
-    // TODO: gc시 mark된 block이 11632046개라는 데 맞나?
     #[test]
     #[serial] // Ralloc은 동시에 두 개의 pool 사용할 수 없기 때문에 테스트를 병렬적으로 실행하면 안됨 (Ralloc은 global pool 하나로 관리)
     fn exchange_many() {
