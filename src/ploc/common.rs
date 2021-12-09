43f204de9a37861150a022fd7da3a17f0b0ee33a
Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>
2021-12-09 11:35:45 +0900

    Add checkpoint
    Signed-off-by: Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>

--- src/ploc/common.rs
+++ src/ploc/common.rs
@@ -8,15 +8,16 @@ use std::{
 };
 
 use crossbeam_epoch::Guard;
+use crossbeam_utils::CachePadded;
 
 use crate::{
     pepoch::{atomic::Pointer, PAtomic, PShared},
-    Memento,
     pmem::{
         ll::persist_obj,
         ralloc::{Collectable, GarbageCollection},
         PoolHandle,
     },
+    Memento,
 };
 
 /// TODO: doc
@@ -49,76 +50,151 @@ pub trait DeallocNode<T, N: Node> {
     fn dealloc(&self, target: PShared<'_, N>, guard: &Guard, pool: &PoolHandle);
 }
 
-/// Input으로 주어지는 `save_loc`은 `no_read()`로 세팅되어 있어야 함
-#[derive(Debug)]
-pub struct Load<N: Node + Collectable> {
-    _marker: PhantomData<*const N>,
+/// TODO(doc)
+pub trait Invalid {
+    /// TODO(doc)
+    fn invalid() -> Self;
+
+    /// TODO(doc)
+    fn is_invalid(&self) -> bool;
 }
 
-unsafe impl<N: Node + Collectable + Send + Sync> Send for Load<N> {}
-unsafe impl<N: Node + Collectable + Send + Sync> Sync for Load<N> {}
+/// TODO(doc)
+#[derive(Debug)]
+pub struct Checkpoint<T: Invalid + Clone + Collectable> {
+    saved: CachePadded<T>,
+    _marker: PhantomData<*const T>,
+}
 
-impl<N: Node + Collectable> Default for Load<N> {
+unsafe impl<T: Invalid + Clone + Collectable + Send + Sync> Send for Checkpoint<T> {}
+unsafe impl<T: Invalid + Clone + Collectable + Send + Sync> Sync for Checkpoint<T> {}
+
+impl<T: Invalid + Clone + Collectable> Default for Checkpoint<T> {
     fn default() -> Self {
         Self {
+            saved: CachePadded::from(T::invalid()),
             _marker: Default::default(),
         }
     }
 }
 
-impl<N: Node + Collectable> Collectable for Load<N> {
+impl<T: Invalid + Clone + Collectable> Collectable for Checkpoint<T> {
     fn filter(_: &mut Self, _: &mut GarbageCollection, _: &PoolHandle) {}
 }
 
-impl<N> Memento for Load<N>
+impl<T> Memento for Checkpoint<T>
 where
-    N: 'static + Node + Collectable,
+    T: 'static + Invalid + Clone + Collectable,
 {
     type Object<'o> = ();
-    type Input<'o> = (&'o PAtomic<N>, &'o PAtomic<N>);
-    type Output<'o> = PShared<'o, N>;
+    type Input<'o> = T;
+    type Output<'o> = T;
     type Error<'o> = !;
 
     fn run<'o>(
         &'o mut self,
         (): Self::Object<'o>,
-        (save_loc, point): Self::Input<'o>, // TODO: point는 object로, save_loc은 Load가 들고 있기
+        chk: Self::Input<'o>, // TODO: point는 object로, save_loc은 Load가 들고 있기
         rec: bool,
         guard: &'o Guard,
         _: &'static PoolHandle,
     ) -> Result<Self::Output<'o>, Self::Error<'o>> {
         if rec {
-            if let Some(saved) = self.result(save_loc, guard) {
+            if let Some(saved) = self.result() {
                 return Ok(saved);
             }
         }
 
         // Normal run
-        let p = point.load(Ordering::SeqCst, guard);
-        save_loc.store(p, Ordering::Relaxed);
-        persist_obj(save_loc, true);
-        Ok(p)
+        self.saved = CachePadded::from(chk.clone());
+        persist_obj(&self.saved, true);
+        Ok(chk)
     }
 
     fn reset(&mut self, _: &Guard, _: &'static PoolHandle) {}
 }
 
-impl<N: Node + Collectable> Load<N> {
+impl<T: Invalid + Clone + Collectable> Checkpoint<T> {
     #[inline]
-    fn result<'g>(&self, save_loc: &PAtomic<N>, guard: &'g Guard) -> Option<PShared<'g, N>> {
-        let saved = save_loc.load(Ordering::Relaxed, guard);
-
-        if saved == Self::no_read() {
+    fn result<'g>(&self) -> Option<T> {
+        if self.saved.is_invalid() {
             None
         } else {
-            Some(saved)
+            Some(*self.saved.clone())
         }
     }
-
-    /// `Read`가 읽은 적이 없다는 걸 표시하기 위한 포인터
-    #[inline]
-    pub fn no_read<'g, T>() -> PShared<'g, T> {
-        const NO_READ: usize = usize::MAX - u32::MAX as usize;
-        unsafe { PShared::<T>::from_usize(NO_READ) }
-    }
 }
+
+// /// Input으로 주어지는 `save_loc`은 `no_read()`로 세팅되어 있어야 함
+// #[derive(Debug)]
+// pub struct Load<T: Collectable> {
+//     _marker: PhantomData<*const N>,
+// }
+
+// unsafe impl<T: Collectable + Send + Sync> Send for Checkpoint<T> {}
+// unsafe impl<T: Collectable + Send + Sync> Sync for Checkpoint<T> {}
+
+// impl<T: Collectable> Default for Checkpoint<T> {
+//     fn default() -> Self {
+//         Self {
+//             _marker: Default::default(),
+//         }
+//     }
+// }
+
+// impl<T: Collectable> Collectable for Checkpoint<T> {
+//     fn filter(_: &mut Self, _: &mut GarbageCollection, _: &PoolHandle) {}
+// }
+
+// impl<T> Memento for Checkpoint<T>
+// where
+//     N: 'static + Node,
+// {
+//     type Object<'o> = ();
+//     type Input<'o> = (&'o PAtomic<T>, &'o PAtomic<T>);
+//     type Output<'o> = PShared<'o, N>;
+//     type Error<'o> = !;
+
+//     fn run<'o>(
+//         &'o mut self,
+//         (): Self::Object<'o>,
+//         (save_loc, point): Self::Input<'o>, // TODO: point는 object로, save_loc은 Load가 들고 있기
+//         rec: bool,
+//         guard: &'o Guard,
+//         _: &'static PoolHandle,
+//     ) -> Result<Self::Output<'o>, Self::Error<'o>> {
+//         if rec {
+//             if let Some(saved) = self.result(save_loc, guard) {
+//                 return Ok(saved);
+//             }
+//         }
+
+//         // Normal run
+//         let p = point.load(Ordering::SeqCst, guard);
+//         save_loc.store(p, Ordering::Relaxed);
+//         persist_obj(save_loc, true);
+//         Ok(p)
+//     }
+
+//     fn reset(&mut self, _: &Guard, _: &'static PoolHandle) {}
+// }
+
+// impl<T: Collectable> Checkpoint<T> {
+//     #[inline]
+//     fn result<'g>(&self, save_loc: &PAtomic<T>, guard: &'g Guard) -> Option<PShared<'g, N>> {
+//         let saved = save_loc.load(Ordering::Relaxed, guard);
+
+//         if saved == Self::no_read() {
+//             None
+//         } else {
+//             Some(saved)
+//         }
+//     }
+
+//     /// `Read`가 읽은 적이 없다는 걸 표시하기 위한 포인터
+//     #[inline]
+//     pub fn no_read<'g, T>() -> PShared<'g, T> {
+//         const NO_READ: usize = usize::MAX - u32::MAX as usize;
+//         unsafe { PShared::<T>::from_usize(NO_READ) }
+//     }
+// }
