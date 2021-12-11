439355004159bf130d10dc1881d060a0052b1051
Seungmin Jeon <seungmin.jeon@cp.kaist.ac.kr>
2021-12-11 06:22:38 +0000

    Enable evaluation of clevel-rust concurrent

--- evaluation/HashEval/hash/Clevel-rust/clevel_rust.cpp
+++ evaluation/HashEval/hash/Clevel-rust/clevel_rust.cpp
@@ -21,24 +21,20 @@
 typedef uint64_t Key;
 typedef uint64_t Value;
 
-const uint IX_OBJ = 0;
-const uint IX_NR_MEMENTO = 1;
-const uint IX_MEMENTO_START = 2;
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-    typedef struct _poolhandle PoolHandle;
-    typedef struct _clevel ClevelRust;
-    typedef struct _memento ClevelMemento;
 
-    PoolHandle *pool_create(char *path, size_t size, int tnum);
-    void *get_root(size_t ix, PoolHandle *pool);
-    bool run_search(ClevelMemento *m, ClevelRust *obj, Key k, PoolHandle *pool);
-    bool run_insert(ClevelMemento *m, ClevelRust *obj, Key k, Value v, PoolHandle *pool);
-    bool run_update(ClevelMemento *m, ClevelRust *obj, Key k, Value v, PoolHandle *pool);
-    bool run_delete(ClevelMemento *m, ClevelRust *obj, Key k, PoolHandle *pool);
+    typedef struct _custom_clevel ClevelRust;
+
+    ClevelRust *clevel_new(int tnum);
+    void clevel_free(ClevelRust *o);
+    bool clevel_search(ClevelRust *o, Key k, unsigned tid);
+    bool clevel_insert(ClevelRust *o, Key k, Value v, unsigned tid);
+    bool clevel_update(ClevelRust *o, Key k, Value v, unsigned tid);
+    bool clevel_delete(ClevelRust *o, Key k, unsigned tid);
+    size_t clevel_get_capacity(ClevelRust *c);
 
 #ifdef __cplusplus
 }
@@ -47,34 +43,31 @@ extern "C"
 
 using namespace std;
 
+uint64_t inserted = 0;
 class CLevelMemento : public hash_api
 {
-    PoolHandle *pool;
     ClevelRust *c;
-    ClevelMemento **m; // tnum memento*`
 
 public:
     CLevelMemento(int tnum = 1)
     {
-        char *path = "/mnt/pmem0/clevel_memento";
-        const size_t size = 64UL * 1024 * 1024 * 1024;
-        pool = pool_create(path, size, tnum);
-        c = reinterpret_cast<ClevelRust *>(get_root(IX_OBJ, pool));
-        m = (ClevelMemento **)malloc(sizeof(ClevelMemento *) * tnum);
-        for (int tid = 0; tid < tnum; ++tid)
-        {
-            m[tid] = reinterpret_cast<ClevelMemento *>(get_root(IX_MEMENTO_START + tid, pool));
-        }
+        c = clevel_new(tnum);
     }
-    ~CLevelMemento(){};
-    std::string hash_name()
+    ~CLevelMemento(void)
     {
-        return "clevel-memento";
+        clevel_free(c);
     };
+    std::string hash_name() { return "clevel-memento"; };
+    hash_Utilization utilization()
+    {
+        hash_Utilization h;
+        h.load_factor = (float)inserted / clevel_get_capacity(c);
+        return h;
+    }
     bool find(const char *key, size_t key_sz, char *value_out, unsigned tid)
     {
         auto k = *reinterpret_cast<const Key *>(key);
-        return run_search(m[tid], c, k, pool);
+        return clevel_search(c, k, tid);
     }
 
     bool insert(const char *key, size_t key_sz, const char *value,
@@ -82,31 +75,36 @@ public:
     {
         auto k = *reinterpret_cast<const Key *>(key);
         auto v = *reinterpret_cast<const Value *>(value);
-        return run_insert(m[tid], c, k, v, pool);
+        bool ret = clevel_insert(c, k, v, tid);
+        if (ret)
+        {
+            inserted += 1;
+        }
+        return ret;
     }
     bool insertResize(const char *key, size_t key_sz, const char *value,
                       size_t value_sz, unsigned tid, unsigned t)
     {
         auto k = *reinterpret_cast<const Key *>(key);
         auto v = *reinterpret_cast<const Value *>(value);
-        return run_insert(m[tid], c, k, v, pool);
+        return clevel_insert(c, k, v, tid);
     }
     bool update(const char *key, size_t key_sz, const char *value,
                 size_t value_sz)
     {
-        // return true same as clevel c++.
+        // clevel c++과 동일하게 return true. 어차피 실험은 insert, delete, search만 함
         return true;
     }
 
     bool remove(const char *key, size_t key_sz, unsigned tid)
     {
         auto k = *reinterpret_cast<const Key *>(key);
-        return run_delete(m[tid], c, k, pool);
+        return clevel_delete(c, k, tid);
     }
 
     int scan(const char *key, size_t key_sz, int scan_sz, char *&values_out)
     {
-        // return scan_sz same as clevel c++.
+        // clevel c++과 동일하게 return scan_sz
         return scan_sz;
     }
 };
