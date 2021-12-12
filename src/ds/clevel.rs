2411c2717a0b43dba67f224af37cee60c093562a
Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>
2021-12-12 18:39:35 +0900

    Revert "context switch wip"
    This reverts commit d227220f0c5441c1139d197d1e747775f961e14c.

--- src/ds/clevel.rs
+++ src/ds/clevel.rs
@@ -28,11 +28,9 @@ use crate::node::Node;
 use crate::pepoch::PShared;
 use crate::pepoch::{PAtomic, PDestroyable, POwned};
 use crate::ploc::Insert;
-use crate::ploc::NeedRetry;
 use crate::ploc::SMOAtomic;
 use crate::ploc::Traversable;
 use crate::ploc::Update;
-use crate::ploc::UpdateDeleteInfo;
 use crate::pmem::global_pool;
 use crate::pmem::persist_obj;
 use crate::pmem::Collectable;
@@ -155,14 +153,14 @@ trait AddLevel<K, V> {
 
 #[derive(Debug)]
 struct ContextSwitch<K, V> {
-    update: Update<ClevelInner<K, V>, Node<Context<K, V>>, Self>,
+    // update: Update<SMOAtomic..., Node<PAtomic<[MaybeUninit<Bucket<K, V>>]>>>,
     _marker: PhantomData<(K, V)>,
 }
 
 impl<K, V> Default for ContextSwitch<K, V> {
     fn default() -> Self {
         Self {
-            update: Default::default(),
+            // update: Default::default(),
             _marker: Default::default(),
         }
     }
@@ -175,56 +173,24 @@ impl<K, V> Collectable for ContextSwitch<K, V> {
 }
 
 impl<K: 'static + PartialEq + Hash, V: 'static> Memento for ContextSwitch<K, V> {
-    type Object<'o> = &'o ClevelInner<K, V>;
-    type Input<'o> = PShared<'o, Node<Context<K, V>>>;
+    type Object<'o> = &'o PAtomic<Node<PAtomic<[MaybeUninit<Bucket<K, V>>]>>>;
+    type Input<'o> = ();
     type Output<'o> = ();
     type Error<'o> = !;
 
     fn run<'o>(
         &mut self,
         inner: Self::Object<'o>,
-        context_new: Self::Input<'o>,
+        _: Self::Input<'o>,
         rec: bool,
         guard: &'o Guard,
         pool: &'static PoolHandle,
     ) -> Result<Self::Output<'o>, Self::Error<'o>> {
-        // self.update.run()
+        todo!()
     }
 
     fn reset(&mut self, guard: &Guard, pool: &'static PoolHandle) {
         // TODO
-    }
-}
-
-impl<K, V> UpdateDeleteInfo<ClevelInner<K, V>, Node<Context<K, V>>>
-    for ContextSwitch<K, V>
-{
-    fn prepare_delete<'g>(
-        _: PShared<'_, Node<Context<K, V>>>,
-        _: PShared<'_, Node<Context<K, V>>>,
-        _: &ClevelInner<K, V>,
-        _: &'g Guard,
-        _: &PoolHandle,
-    ) -> Result<Option<PShared<'g, Node<Context<K, V>>>>, NeedRetry> {
-        panic!("not used.")
-    }
-
-    fn prepare_update<'g>(
-        cur: PShared<'_, Node<Context<K, V>>>,
-        expected: PShared<'_, Node<Context<K, V>>>,
-        _: &ClevelInner<K, V>,
-        _: &'g Guard,
-        _: &PoolHandle,
-    ) -> bool {
-        cur == expected
-    }
-
-    fn node_when_deleted<'g>(
-        _: PShared<'_, Node<Context<K, V>>>,
-        _: &'g Guard,
-        _: &PoolHandle,
-    ) -> PShared<'g, Node<Context<K, V>>> {
-        panic!("not used.")
     }
 }
 
