b260461b32f9ca382d9eb1e284f67c0368291dcd
Seungmin Jeon <seungmin.jeon@cp.kaist.ac.kr>
2022-01-06 13:15:27 +0000

    eval: enable pmdk_queue pair bench

---
+++ evaluation/src/pmdk/queue.cpp
@@ -0,0 +1,113 @@
+#include <libpmemobj++/p.hpp>
+#include <libpmemobj++/persistent_ptr.hpp>
+#include <libpmemobj++/make_persistent.hpp>
+#include <libpmemobj++/pool.hpp>
+#include <libpmemobj++/transaction.hpp>
+#include <libpmemobj++/mutex.hpp>
+#include <iostream>
+
+#include "queue.hpp"
+
+using namespace pmem::obj;
+using namespace std;
+
+void queue::push(pool_base &pop, uint64_t value)
+{
+    transaction::run(
+        pop, [&]
+        {
+            auto n = make_persistent<node>();
+            n->value = value;
+            n->next = nullptr;
+            if (head == nullptr && tail == nullptr)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail->next = n;
+                tail = n;
+            }
+        },
+        pmutex);
+}
+
+std::optional<int> queue::pop(pool_base &pop)
+{
+    std::optional<int> value = std::nullopt;
+    transaction::run(
+        pop, [&]
+        {
+            if (head == nullptr)
+            {
+                return; // EMPTY
+            }
+            value = head->value;
+            auto next = head->next;
+            delete_persistent<node>(head);
+            head = next;
+            if (head == nullptr)
+                tail = nullptr;
+        },
+        pmutex);
+    return value;
+}
+
+void queue::show(void) const
+{
+    for (auto n = head; n != nullptr; n = n->next)
+        std::cout << n->value << " ";
+    std::cout << std::endl;
+}
+
+int get_queue_pair_nops(string filepath, int nr_threads, float duration)
+{
+    remove(filepath.c_str());
+    auto pop = pool<queue>::create(filepath, "MY_LAYOUT", ((size_t)POOL_SIZE));
+    persistent_ptr<queue> q = pop.root();
+
+    // Initailize
+    for (int i = 0; i < PIPE_INIT_SIZE; i++)
+    {
+        q->push(pop, i);
+    }
+
+    std::thread workers[nr_threads];
+    int local_ops[nr_threads];
+    int sum_ops = 0;
+
+    // `duration`초 동안 pair {push; pop;} 수행 횟수 카운트
+    // TODO: generic하게 구현 (common.rs의 TestNOps trait처럼)
+    for (int tid = 0; tid < nr_threads; tid++)
+    {
+        workers[tid] = std::thread(
+            [](
+                int tid, float duration, int &local_ops, pool<queue> pop, persistent_ptr<queue> q)
+            {
+                local_ops = 0;
+                struct timespec begin, end;
+                clock_gettime(CLOCK_REALTIME, &begin);
+                while (true)
+                {
+                    clock_gettime(CLOCK_REALTIME, &end);
+                    long elapsed = end.tv_sec - begin.tv_sec;
+                    if (duration < elapsed)
+                    {
+                        break;
+                    }
+
+                    q->push(pop, tid);
+                    q->pop(pop);
+                    local_ops += 1;
+                }
+            },
+            tid, duration, std::ref(local_ops[tid]), pop, q);
+    }
+
+    for (int tid = 0; tid < nr_threads; ++tid)
+    {
+        workers[tid].join();
+        sum_ops += local_ops[tid];
+    }
+    return sum_ops;
+}
--- evaluation/src/pmdk/queue.hpp
+++ evaluation/src/pmdk/queue.hpp
@@ -1,3 +1,4 @@
+// #pragma once
 
 #include <libpmemobj++/p.hpp>
 #include <libpmemobj++/persistent_ptr.hpp>
@@ -6,54 +7,32 @@
 #include <libpmemobj++/transaction.hpp>
 #include <libpmemobj++/mutex.hpp>
 #include <iostream>
+#include "../common.hpp"
 
 using namespace pmem::obj;
+using namespace std;
 
-class queue {
-   struct node {
-       p<int> value; // TODO: generic
-       persistent_ptr<node> next;
-   };
+#ifndef PMDK_QUEUE
+#define PMDK_QUEUE
+
+class queue
+{
+    struct node
+    {
+        p<int> value; // TODO: generic
+        persistent_ptr<node> next;
+    };
+
 private:
     pmem::obj::mutex pmutex;
     persistent_ptr<node> head;
     persistent_ptr<node> tail;
-public:
-    void push(pool_base &pop, uint64_t value)
-    {
-        transaction::run(pop, [&] {
-            auto n = make_persistent<node>();
-            n->value = value;
-            n->next = nullptr;
-            if (head == nullptr && tail == nullptr) {
-				head = tail = n;
-			} else {
-				tail->next = n;
-				tail = n;
-			}
-        }, pmutex);
-    }
-   std::optional<int> pop(pool_base &pop)
-   {
-        std::optional<int> value = std::nullopt;
-        transaction::run(pop, [&] {
-            if (head == nullptr) {
-                return; // EMPTY
-            }
-            value = head->value;
-            auto next = head->next;
-            delete_persistent<node>(head);
-            head = next;
-			if (head == nullptr)
-				tail = nullptr;
-        }, pmutex);
-        return value;
-   }
 
-   void show(void) const
-   {
-       for (auto n = head; n != nullptr; n = n->next)
-           std::cout << n->value << " ";
-       std::cout << std::endl;
-   }
+public:
+    void push(pool_base &pop, uint64_t value);
+    std::optional<int> pop(pool_base &pop);
+    void show(void) const;
 };
+
+int get_queue_pair_nops(string filepath, int nr_threads, float duration);
+#endif
