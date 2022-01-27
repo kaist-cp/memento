9108f032c5d7912e63fd51265f39480a028dcb6a
Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>
2022-01-27 18:08:09 +0900

    queue: Add coditional persist queue
    Signed-off-by: Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>

--- src/ds/queue_lp.rs
+++ src/ds/queue_lp.rs
@@ -255,7 +255,7 @@ impl<T: Clone + Collectable> Collectable for Queue<T> {
 
 impl<T: Clone + Collectable> Traversable<Node<T>> for Queue<T> {
     fn search(&self, target: PShared<'_, Node<T>>, guard: &Guard, pool: &PoolHandle) -> bool {
-        let mut curr = self.head.load(false, Ordering::SeqCst, guard);
+        let mut curr = self.head.load_lp(Ordering::SeqCst, guard);
 
         // TODO(opt): null 나올 때까지 하지 않고 tail을 통해서 범위를 제한할 수 있을지?
         while !curr.is_null() {
@@ -264,7 +264,7 @@ impl<T: Clone + Collectable> Traversable<Node<T>> for Queue<T> {
             }
 
             let curr_ref = unsafe { curr.deref(pool) };
-            curr = curr_ref.next.load(false, Ordering::SeqCst, guard);
+            curr = curr_ref.next.load_lp(Ordering::SeqCst, guard);
         }
 
         false
@@ -283,14 +283,14 @@ impl<T: Clone + Collectable> Queue<T> {
         let (tail, tail_ref) = loop {
             let tail = self.tail.load(Ordering::SeqCst, guard);
             let tail_ref = unsafe { tail.deref(pool) }; // TODO(must): filter 에서 tail align 해야 함
-            let next = tail_ref.next.load(false, Ordering::SeqCst, guard);
+            let next = tail_ref.next.load_lp(Ordering::SeqCst, guard);
 
             if next.is_null() {
                 break (tail, tail_ref);
             }
 
             // tail is stale
-            let next = tail_ref.next.load(true, Ordering::SeqCst, guard);
+            persist_obj(&tail_ref.next, false);
             let _ =
                 self.tail
                     .compare_exchange(tail, next, Ordering::SeqCst, Ordering::SeqCst, guard);
@@ -298,7 +298,7 @@ impl<T: Clone + Collectable> Queue<T> {
 
         if tail_ref
             .next
-            .insert::<_, REC>(node, self, &mut try_enq.insert, guard, pool)
+            .insert_lp::<_, REC>(node, self, &mut try_enq.insert, guard, pool)
             .is_err()
         {
             return Err(TryFail);
@@ -361,9 +361,9 @@ impl<T: Clone + Collectable> Queue<T> {
         pool: &PoolHandle,
     ) -> Result<Option<T>, TryFail> {
         let (head, next) = loop {
-            let head = self.head.load(false, Ordering::SeqCst, guard);
+            let head = self.head.load_lp(Ordering::SeqCst, guard);
             let head_ref = unsafe { head.deref(pool) };
-            let next = head_ref.next.load(false, Ordering::SeqCst, guard);
+            let next = head_ref.next.load_lp(Ordering::SeqCst, guard); // TODO(opt): 여기서 load하지 않고 head_next를 peek해보고 나중에 할 수도 있음
             let tail = self.tail.load(Ordering::SeqCst, guard);
 
             if head != tail || next.is_null() {
@@ -371,7 +371,7 @@ impl<T: Clone + Collectable> Queue<T> {
             }
 
             // tail is stale
-            let next = head_ref.next.load(true, Ordering::SeqCst, guard);
+            persist_obj(&unsafe { tail.deref(pool) }.next, false);
             let _ =
                 self.tail
                     .compare_exchange(tail, next, Ordering::SeqCst, Ordering::SeqCst, guard);
@@ -527,7 +527,7 @@ mod test {
     //      - 여기서 +2는 Root, Queue를 가리키는 포인터
     #[test]
     fn enq_deq() {
-        const FILE_NAME: &str = "queue_enq_deq.pool";
+        const FILE_NAME: &str = "queue_lp_enq_deq.pool";
         const FILE_SIZE: usize = 8 * 1024 * 1024 * 1024;
 
         run_test::<TestRootObj<Queue<usize>>, EnqDeq, _>(FILE_NAME, FILE_SIZE, NR_THREAD + 1)
