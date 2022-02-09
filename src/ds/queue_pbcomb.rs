ed0fb3fd9e20b1ea0fc50b84883781c95ba6e562
Seungmin Jeon <seungmin.jeon@cp.kaist.ac.kr>
2022-02-09 08:58:47 +0000

    pbcomb: Implement enq & deq inner

--- src/ds/queue_pbcomb.rs
+++ src/ds/queue_pbcomb.rs
@@ -76,7 +76,7 @@ static mut OLD_TAIL: PPtr<Node> = PPtr::null(); // TODO: initially, &DUMMY
 lazy_static::lazy_static! {
     // static mut TO_PERSIST: Set<*mut Node>;  // TODO: initiallay, empty set
 
-    /// Used PBQueueENQ instance of PBCOMB
+    /// Used by the PBQueueENQ instance of PBCOMB
     static ref E_LOCK: AtomicUsize = AtomicUsize::new(0);
 
     /// Used by the PBQueueDEQ instance of PBCOMB
@@ -119,28 +119,42 @@ impl QueuePBComb {
     /// normal run
     ///
     /// enq or deq 실행
-    pub fn PBQueue(&mut self, func: Func, arg: Data, seq: usize, tid: usize) -> ReturnVal {
+    pub fn PBQueue(
+        &mut self,
+        func: Func,
+        arg: Data,
+        seq: usize,
+        tid: usize,
+        pool: &PoolHandle,
+    ) -> ReturnVal {
         match func {
-            Func::ENQUEUE => self.PBQueueEnq(arg, seq, tid),
-            Func::DEQUEUE => self.PBQueueDnq(seq, tid),
+            Func::ENQUEUE => self.PBQueueEnq(arg, seq, tid, pool),
+            Func::DEQUEUE => self.PBQueueDnq(seq, tid, pool),
         }
     }
 
     /// recovery run
     ///
     /// 최근 crash난 enq or deq를 재실행 (exactly-once)
-    pub fn recover(&mut self, func: Func, arg: Data, seq: usize, tid: usize) -> ReturnVal {
+    pub fn recover(
+        &mut self,
+        func: Func,
+        arg: Data,
+        seq: usize,
+        tid: usize,
+        pool: &PoolHandle,
+    ) -> ReturnVal {
         match func {
             Func::ENQUEUE => {
                 // 1. check seq number and re-announce if request is not yet announced
                 if self.e_request[tid].seq != seq {
-                    return self.PBQueue(func, arg, seq, tid);
+                    return self.PBQueue(func, arg, seq, tid, pool);
                 }
 
                 // 2. check activate and re-execute if request is not yet applied
                 let e_state = &self.e_state[self.e_index];
                 if self.e_request[tid].activate != e_state.deactivate[tid] {
-                    return self.PerformEnqReq(tid); // TODO: arg
+                    return self.PerformEnqReq(tid, pool); // TODO: arg
                 }
 
                 // 3. return value if request is already applied
@@ -149,13 +163,13 @@ impl QueuePBComb {
             Func::DEQUEUE => {
                 // 1. check seq number and re-announce if request is not yet announced
                 if self.d_request[tid].seq != seq {
-                    return self.PBQueue(func, arg, seq, tid);
+                    return self.PBQueue(func, arg, seq, tid, pool);
                 }
 
                 // 2. check activate and re-execute if request is not yet applied
                 let d_state = &self.d_state[self.d_index];
                 if self.d_request[tid].activate != d_state.deactivate[tid] {
-                    return self.PerformDeqReq(tid); // TODO: arg
+                    return self.PerformDeqReq(tid, pool); // TODO: arg
                 }
 
                 // 3. return value if request is already applied
@@ -168,7 +182,7 @@ impl QueuePBComb {
 /// Enq
 impl QueuePBComb {
     /// enqueue 요청 등록 후 실행 (thread-local)
-    fn PBQueueEnq(&mut self, arg: Data, seq: usize, tid: usize) -> ReturnVal {
+    fn PBQueueEnq(&mut self, arg: Data, seq: usize, tid: usize, pool: &PoolHandle) -> ReturnVal {
         // 요청 등록
         self.e_request[tid] = RequestRec {
             func: Func::ENQUEUE,
@@ -178,11 +192,11 @@ impl QueuePBComb {
         };
 
         // 실행
-        self.PerformEnqReq(tid)
+        self.PerformEnqReq(tid, pool)
     }
 
     /// enqueue 요청 실행 (thread-local)
-    fn PerformEnqReq(&mut self, tid: usize) -> ReturnVal {
+    fn PerformEnqReq(&mut self, tid: usize, pool: &PoolHandle) -> ReturnVal {
         // enq combiner 결정
         loop {
             let lval = E_LOCK.load(Ordering::SeqCst);
@@ -214,7 +228,7 @@ impl QueuePBComb {
             let t_req = &self.e_request[t];
             if t_req.activate != self.e_state[ind].deactivate[t] {
                 // TODO: add EState[ind].tail to `toPersist`
-                self.enqueue(&self.e_state[ind].tail, t_req.arg);
+                Self::enqueue(&mut self.e_state[ind].tail, t_req.arg, pool);
                 self.e_state[ind].return_val[t] = Some(ReturnVal::EnqRetVal(()));
                 self.e_state[ind].deactivate[t] = t_req.activate;
             }
@@ -233,15 +247,22 @@ impl QueuePBComb {
 
     /// 실질적인 enqueue: tail 뒤에 새로운 노드 삽입하고 tail로 설정
     // TODO: arg
-    fn enqueue(&self, tail: &PPtr<Node>, arg: Data) {
-        todo!()
+    fn enqueue(tail: &mut PPtr<Node>, arg: Data, pool: &PoolHandle) {
+        let new_node = pool.alloc::<Node>();
+        let new_node_ref = unsafe { new_node.deref_mut(pool) };
+        new_node_ref.data = arg;
+        new_node_ref.next = PPtr::null();
+
+        let tail_ref = unsafe { tail.deref_mut(pool) };
+        tail_ref.next = new_node;
+        *tail = new_node;
     }
 }
 
 /// Deq
 impl QueuePBComb {
     /// dequeue 요청 등록 후 실행 (thread-local)
-    fn PBQueueDnq(&mut self, seq: usize, tid: usize) -> ReturnVal {
+    fn PBQueueDnq(&mut self, seq: usize, tid: usize, pool: &PoolHandle) -> ReturnVal {
         // 요청 등록
         self.d_request[tid] = RequestRec {
             func: Func::DEQUEUE,
@@ -251,11 +272,11 @@ impl QueuePBComb {
         };
 
         // 실행
-        self.PerformDeqReq(tid)
+        self.PerformDeqReq(tid, pool)
     }
 
     /// dequeue 요청 실행 (thread-local)
-    fn PerformDeqReq(&mut self, tid: usize) -> ReturnVal {
+    fn PerformDeqReq(&mut self, tid: usize, pool: &PoolHandle) -> ReturnVal {
         // deq combiner 결정
         loop {
             let lval = D_LOCK.load(Ordering::SeqCst);
@@ -288,7 +309,7 @@ impl QueuePBComb {
                 let ret_val;
                 // 확실히 persist된 노드들만 deq 수행. OLD_TAIL부터는 현재 enq 중인거라 persist 보장되지 않음
                 if unsafe { OLD_TAIL } != self.d_state[ind].head {
-                    let node = self.dequeue(&self.d_state[ind].head);
+                    let node = Self::dequeue(&mut self.d_state[ind].head, pool);
                     ret_val = ReturnVal::DeqRetVal(Some(node));
                 } else {
                     ret_val = ReturnVal::DeqRetVal(None);
@@ -306,8 +327,16 @@ impl QueuePBComb {
     }
 
     /// 실질적인 dequeue: head 한 칸 전진하고 old head를 반환
-    fn dequeue(&self, head: &PPtr<Node>) -> PPtr<Node> {
-        todo!()
+    fn dequeue(head: &mut PPtr<Node>, pool: &PoolHandle) -> PPtr<Node> {
+        let head_ref = unsafe { head.deref(pool) };
+
+        // NOTE: 얘네 구현은 데이터를 반환하질 않고 노드를 반환.
+        // 노드를 dequeue해간 애는 다 썼다고 함부로 노드 free하면 안됨. queue의 sentinel 노드로 남아있을 수 있음. TODO: 그냥 data 반환해도 될 것같은데..왜지
+        let ret = head_ref.next;
+        if !ret.is_null() {
+            *head = head_ref.next;
+        }
+        ret
     }
 }
 
@@ -348,7 +377,7 @@ mod test {
                     while JOB_FINISHED.load(Ordering::SeqCst) != NR_THREAD {}
 
                     // Check queue is empty
-                    let res = queue.PBQueue(Func::DEQUEUE, tid, 0, tid);
+                    let res = queue.PBQueue(Func::DEQUEUE, tid, 0, tid, pool);
                     if let ReturnVal::DeqRetVal(res) = res {
                         assert!(res.is_none());
                     } else {
@@ -364,10 +393,10 @@ mod test {
                 _ => {
                     // enq; deq;
                     for i in 0..COUNT {
-                        let _ = queue.PBQueue(Func::ENQUEUE, tid, mmt.enq_seq, tid);
+                        let _ = queue.PBQueue(Func::ENQUEUE, tid, mmt.enq_seq, tid, pool);
                         mmt.enq_seq += 1;
                         persist_obj(mmt, true);
-                        let res = queue.PBQueue(Func::DEQUEUE, tid, mmt.deq_seq, tid);
+                        let res = queue.PBQueue(Func::DEQUEUE, tid, mmt.deq_seq, tid, pool);
                         mmt.deq_seq += 1;
                         persist_obj(mmt, true);
 
