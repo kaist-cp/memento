5d74d47727222cd81fcde6cc4e8c562a56092d38
Seungmin Jeon <seungmin.jeon@cp.kaist.ac.kr>
2022-02-14 04:13:02 +0000

    pbcomb: test.sh

--- test.sh
+++ test.sh
@@ -12,16 +12,27 @@ set -e
 #     RUST_BACKTRACE=full RUST_MIN_STACK=10073741824 cargo test --release --features no_persist --features stress insert_update_search -- --nocapture
 # done
 
-# # all
-# while true; do
-#     rm -rf test
-#     RUST_BACKTRACE=full RUST_MIN_STACK=10073741824 cargo test --release --features no_persist --features stress queue_gen -- --nocapture
-#     # RUST_BACKTRACE=full RUST_MIN_STACK=10073741824 cargo test --release --features no_persist --features stress queue_gen -- --nocapture
-# done
+# all
+while true; do
 
-rm -rf /mnt/pmem0/test
-RUST_MIN_STACK=10073741824 cargo test --release soft_list -- --nocapture
-RUST_MIN_STACK=10073741824 cargo test --release soft_hash -- --nocapture
+    # DRAM
+    rm -rf test
+    mkdir test
+    # /home/ubuntu/seungmin.jeon/memento-dev/target/debug/deps/memento-d17820363e14a33a ds::queue_pbcomb::test::enq_deq --exact --nocapture
+
+    # cargo test --features no_persist queue_pbcomb -- --nocapture > ./test/test.json
+    cargo test --release --features no_persist queue_pbcomb -- --nocapture > ./test/out.json
+    # RUST_BACKTRACE=full RUST_MIN_STACK=10073741824 cargo test --release --features no_persist queue_pbcomb -- --nocapture
+
+    # # PMEM
+    # rm -rf /mnt/pmem0/test
+    # RUST_MIN_STACK=10073741824 cargo test --release queue_pbcomb -- --nocapture
+    # # RUST_MIN_STACK=10073741824 cargo test --release queue_pbcomb -- --nocapture
+done
+
+# rm -rf /mnt/pmem0/test
+# RUST_MIN_STACK=10073741824 cargo test --release soft_list -- --nocapture
+# RUST_MIN_STACK=10073741824 cargo test --release soft_hash -- --nocapture
 
 # while true; do
 #     rm -rf /mnt/pmem0/test
