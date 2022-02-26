9787aab70248248730ca4a4d6d8e0f9ef6d6153c
Seungmin Jeon <seungmin.jeon@cp.kaist.ac.kr>
2022-02-27 03:04:52 +0900

    Update fig

--- evaluation/plot.py
+++ evaluation/plot.py
@@ -10,16 +10,16 @@ import traceback
 objs = {
     "queue": {
         "targets": {
-            "memento_queue": {'data_id': '', 'label': "Memento queue", 'marker': 'o', 'color': 'k', 'style': '-'},
-            "memento_queue_lp": {'data_id': '', 'label': "Memento queue-lp", 'marker': 'o', 'color': 'k', 'style': ':'},
-            "memento_queue_general": {'data_id': '', 'label': "Memento queue-general", 'marker': 'o', 'color': 'k', 'style': '--'},
-            "memento_queue_pbcomb": {'data_id': '', 'label': "Memento queue-pbcomb", 'marker': 'o', 'color': 'k', 'style': '-.'},
-            'durable_queue': {'data_id': '', 'label': "Durable queue", 'marker': 'd', 'color': 'hotpink', 'style': '--'},
-            'log_queue': {'data_id': '', 'label': "Log queue", 'marker': 'x', 'color': 'c', 'style': '--'},
-            'dss_queue': {'data_id': '', 'label': "DSS queue", 'marker': 'v', 'color': 'orange', 'style': '--'},
-            'pbcomb_queue': {'data_id': '', 'label': "PBComb queue", 'marker': '>', 'color': 'red', 'style': '--'},
-            'pmdk_queue': {'data_id': '', 'label': "PMDK queue", 'marker': 's', 'color': 'skyblue', 'style': '--'},
-            'crndm_queue': {'data_id': '', 'label': "Corundum queue", 'marker': '^', 'color': 'green', 'style': '--'},
+            "memento_queue": {'data_id': '', 'label': "MMT", 'marker': 'o', 'color': 'k', 'style': '-'},
+            "memento_queue_lp": {'data_id': '', 'label': "MMT-lp", 'marker': 'o', 'color': 'k', 'style': ':'},
+            "memento_queue_general": {'data_id': '', 'label': "MMT-general", 'marker': 'o', 'color': 'k', 'style': '--'},
+            "memento_queue_pbcomb": {'data_id': '', 'label': "MMT-pbcomb", 'marker': 'o', 'color': 'k', 'style': '-.'},
+            'durable_queue': {'data_id': '', 'label': "Durable", 'marker': 'd', 'color': 'hotpink', 'style': '--'},
+            'log_queue': {'data_id': '', 'label': "Log", 'marker': 'x', 'color': 'c', 'style': '--'},
+            'dss_queue': {'data_id': '', 'label': "DSS", 'marker': 'v', 'color': 'orange', 'style': '--'},
+            'pbcomb_queue': {'data_id': '', 'label': "PBComb", 'marker': '>', 'color': 'red', 'style': '--'},
+            'pmdk_queue': {'data_id': '', 'label': "PMDK", 'marker': 's', 'color': 'skyblue', 'style': '--'},
+            'crndm_queue': {'data_id': '', 'label': "Corundum", 'marker': '^', 'color': 'green', 'style': '--'},
         },
     },
 
@@ -27,8 +27,18 @@ objs = {
 }
 
 
+def draw_legend(line, label, figpath):
+    plt.clf()
+    legendFig = plt.figure("Legend plot")
+    legendFig.legend(line, label, loc='center',
+                     ncol=len(line))
+    legendFig.savefig(figpath, bbox_inches='tight')
+    print(figpath)
+
+
 def draw(xlabel, ylabel, datas, output, x_interval=4):
     plt.clf()
+    plt.figure(figsize=(4, 4))
     markers_on = (datas[0]['x'] == 1) | (datas[0]['x'] % x_interval == 0)
 
     for data in datas:
@@ -37,13 +47,15 @@ def draw(xlabel, ylabel, datas, output, x_interval=4):
     ax = plt.subplot()
     ax.xaxis.set_major_locator(plt.MultipleLocator(x_interval))  # 눈금선 간격
     plt.grid(True)
-    plt.legend()
     plt.xlabel(xlabel, size='large')
     if ylabel != '':
         plt.ylabel(ylabel, size='large')
-    fig_path = "{}.png".format(output)
-    plt.savefig(fig_path, dpi=300)
-    print(fig_path)
+    figpath = "{}.png".format(output)
+    plt.tight_layout()
+    plt.savefig(figpath, bbox_inches='tight', pad_inches=0.02, dpi=300)
+
+    print(figpath)
+    return ax
 
 
 for obj in objs:
@@ -63,6 +75,8 @@ for obj in objs:
             data_path = "./out/{}_{}.csv".format(t, commit.hexsha[:7])
             if exists(data_path):
                 break
+        if data_id != '':
+            data_path = "./out/{}_{}.csv".format(t, data_id)
 
         print("read {} for target {}".format(data_path, t))
         data = data.append(pd.read_csv(data_path))
@@ -111,5 +125,7 @@ for obj in objs:
             ylabel = 'Throughput (M op/s)'
         else:
             ylabel = ''
-        draw('Threads', ylabel,
-             plot_lines, "./out/{}".format(plot_id), 4)
+        ax = draw('Threads', ylabel,
+                  plot_lines, "./out/{}".format(plot_id), 4)
+    axLine, axLabel = ax.get_legend_handles_labels()
+    draw_legend(axLine, axLabel, "./out/{}-legend.png".format(obj))
