846bc27d7ead03fc3a2f5e6f199893af25788adc
Seungmin Jeon <seungmin.jeon@cp.kaist.ac.kr>
2022-06-22 13:27:29 +0000

    eval cas: Apply multiple locations to nrl cas

--- evaluation/cas/src/nrlcas.rs
+++ evaluation/cas/src/nrlcas.rs
@@ -1,19 +1,31 @@
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 use crossbeam_epoch::{unprotected, Guard};
-use crossbeam_utils::CachePadded;
 use evaluation::common::{DURATION, MAX_THREADS, TOTAL_NOPS};
 use memento::{
-    pepoch::PAtomic,
+    pepoch::{atomic::Pointer, PAtomic, PShared},
     pmem::{persist_obj, Collectable, GarbageCollection, PoolHandle, RootObj},
     PDefault,
 };
 
-use crate::{TestNOps, TOTAL_NOPS_FAILED};
+use crate::{cas_random_loc, Locations, Node, TestNOps, TestableCas, TOTAL_NOPS_FAILED};
+
+pub struct NRLLoc {
+    c: PAtomic<Node>, // C: location (val with tid tag)
+    r: [[AtomicUsize; MAX_THREADS + 1]; MAX_THREADS + 1], // R: msgs for location ([N][N])
+}
+
+impl Default for NRLLoc {
+    fn default() -> Self {
+        Self {
+            c: Default::default(),
+            r: array_init::array_init(|_| array_init::array_init(|_| Default::default())),
+        }
+    }
+}
 
 pub struct TestNRLCas {
-    loc: PAtomic<usize>,                              // C: val with tid tag
-    loc_r: [[AtomicUsize; MAX_THREADS]; MAX_THREADS], // R: [N][N]  // TODO: loc 하나당 r 하나
+    locs: Locations<NRLLoc>,
 }
 
 impl Collectable for TestNRLCas {
@@ -23,19 +35,26 @@ impl Collectable for TestNRLCas {
 }
 
 impl PDefault for TestNRLCas {
-    fn pdefault(_: &PoolHandle) -> Self {
+    fn pdefault(pool: &PoolHandle) -> Self {
         Self {
-            loc: Default::default(),
-            loc_r: array_init::array_init(|_| array_init::array_init(|_| Default::default())),
+            locs: Locations::pdefault(pool),
         }
     }
 }
 
 impl TestNOps for TestNRLCas {}
 
+impl TestableCas for TestNRLCas {
+    type Input = usize; // tid
+    type Location = NRLLoc;
+
+    fn cas(&self, tid: Self::Input, loc: &Self::Location, _: &Guard, _: &PoolHandle) -> bool {
+        nrl_cas(loc, tid)
+    }
+}
+
 #[derive(Default, Debug)]
 pub struct TestNRLCasMmt {
-    value: CachePadded<PAtomic<usize>>,
     // TODO: value를 구분하기 위한 per-thread seq num 추가.
     //
     // - "Our recoverable read-write object algorithm assumes that all values
@@ -47,14 +66,6 @@ pub struct TestNRLCasMmt {
     // and does not require special treatment."
 }
 
-impl PDefault for TestNRLCasMmt {
-    fn pdefault(pool: &PoolHandle) -> Self {
-        Self {
-            value: CachePadded::new(PAtomic::new(0, pool)),
-        }
-    }
-}
-
 impl Collectable for TestNRLCasMmt {
     fn filter(_: &mut Self, _: usize, _: &mut GarbageCollection, _: &mut PoolHandle) {
         todo!()
@@ -62,10 +73,15 @@ impl Collectable for TestNRLCasMmt {
 }
 
 impl RootObj<TestNRLCasMmt> for TestNRLCas {
-    fn run(&self, local: &mut TestNRLCasMmt, tid: usize, _: &Guard, pool: &PoolHandle) {
+    fn run(&self, _: &mut TestNRLCasMmt, tid: usize, _: &Guard, pool: &PoolHandle) {
         let duration = unsafe { DURATION };
+        let locs_ref = unsafe { self.locs.as_ref(unprotected(), pool) };
 
-        let (ops, failed) = self.test_nops(&|tid| nrl_cas(tid, &self, local, pool), tid, duration);
+        let (ops, failed) = self.test_nops(
+            &|tid| cas_random_loc(self, tid, locs_ref, unsafe { unprotected() }, pool),
+            tid,
+            duration,
+        );
 
         let _ = TOTAL_NOPS.fetch_add(ops, Ordering::SeqCst);
         let _ = TOTAL_NOPS_FAILED.fetch_add(failed, Ordering::SeqCst);
@@ -74,49 +90,51 @@ impl RootObj<TestNRLCasMmt> for TestNRLCas {
 
 const NULL: usize = 0;
 
-fn nrl_cas(tid: usize, shared: &TestNRLCas, local: &TestNRLCasMmt, pool: &PoolHandle) -> bool {
+fn nrl_cas(loc: &NRLLoc, tid: usize) -> bool {
     let guard = unsafe { unprotected() };
-    let old = nrl_read(shared, guard, pool);
+    let old = nrl_read(loc, guard);
     let new = tid; // TODO: 다양한 new 값
-    nrl_cas_inner(old, new, tid, shared, local, guard, pool)
+    nrl_cas_inner(old, new, loc, tid, guard)
 }
 
-fn nrl_cas_inner<'g>(
-    old_value: usize,
-    new_value: usize,
-    tid: usize,
-    shared: &TestNRLCas,
-    local: &TestNRLCasMmt,
-    guard: &Guard,
-    pool: &PoolHandle,
-) -> bool {
+fn nrl_cas_inner(old: usize, new: usize, loc: &NRLLoc, tid: usize, guard: &Guard) -> bool {
     // Check old
-    let old_p = shared.loc.load(Ordering::SeqCst, guard);
-    if !old_p.is_null() {
-        let (id, val) = (old_p.tid(), unsafe { old_p.deref(pool) });
-        if *val != old_value {
-            return false;
-        }
-        if id != NULL {
-            shared.loc_r[id][tid].store(*val, Ordering::SeqCst);
-            persist_obj(&shared.loc_r[id][tid], true);
-        }
+    let old_p = loc.c.load(Ordering::SeqCst, guard);
+    let (id, val) = decompose(old_p);
+    if val != old {
+        return false;
+    }
+    if id != NULL {
+        loc.r[id][tid].store(val, Ordering::SeqCst);
+        persist_obj(&loc.r[id][tid], true);
     }
 
-    // Make new
-    let mut new_p = local.value.load(Ordering::SeqCst, guard); // TODO: seq로 value 구분
-    unsafe { *(new_p.deref_mut(pool)) = new_value };
-    let new_p = new_p.with_tid(tid);
-
     // CAS
-    let res = shared
-        .loc
-        .compare_exchange(old_p, new_p, Ordering::SeqCst, Ordering::SeqCst, guard)
+    let res = loc
+        .c
+        .compare_exchange(
+            old_p,
+            compose(tid, new),
+            Ordering::SeqCst,
+            Ordering::SeqCst,
+            guard,
+        )
         .is_ok();
-    persist_obj(&shared.loc, true);
+    persist_obj(&loc.c, true);
     res
 }
 
-fn nrl_read(shared: &TestNRLCas, guard: &Guard, pool: &PoolHandle) -> usize {
-    *unsafe { shared.loc.load(Ordering::SeqCst, guard).deref(pool) }
+fn nrl_read(loc: &NRLLoc, guard: &Guard) -> usize {
+    let (_id, value) = decompose(loc.c.load(Ordering::SeqCst, guard));
+    value
+}
+
+// from (id, value) to ptr
+fn compose(tid: usize, value: usize) -> PShared<'static, Node> {
+    unsafe { PShared::from_usize(value) }.with_tid(tid) // TODO: seq로 value 구분
+}
+
+// from ptr to (id, value)
+fn decompose(ptr: PShared<Node>) -> (usize, usize) {
+    (ptr.tid(), ptr.with_tid(NULL).into_usize())
 }
