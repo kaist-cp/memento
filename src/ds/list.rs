9e5ec2ae5e89b6d127a6a86f3af2f1bbadd48ab6
Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>
2022-07-05 21:34:04 +0900

    list: Add harris
    Signed-off-by: Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>

--- src/ds/list.rs
+++ src/ds/list.rs
@@ -31,14 +31,48 @@ impl<T: Collectable> From<(K, V)> for Node<K, V> {
 }
 
 #[derive(Debug)]
-pub struct TryInsert<K, V> {
-    tail: Checkpoint<PAtomic<Node<T>>>,
+pub struct Harris<K, V> {
+    result: Checkpoint<(
+        bool,
+        PPtr<DetectableCASAtomic<K, V>>,
+        PAtomic<Node<K, V>>,
+        PAtomic<Node<K, V>>,
+    )>,
+    help: Cas,
+}
+
+#[derive(Debug)]
+pub struct Find<K, V> {
+    harris: Harris<K, V>,
+}
+
+#[derive(Debug)]
+pub struct Lookup<K, V> {
+    find: Find<K, V>,
+}
+
+#[derive(Debug)]
+pub struct TryInsert {
     insert: Cas,
 }
 
 #[derive(Debug)]
 pub struct Insert<K, V> {
     node: Checkpoint<PAtomic<Node<K, V>>>,
+    find: Find<K, V>,
+    try_ins: TryInsert,
+}
+
+#[derive(Debug)]
+pub struct TryDelete {
+    logical: Cas,
+    physical: Cas,
+}
+
+#[derive(Debug)]
+pub struct Delete<K, V> {
+    find: Find<K, V>,
+    try_del: TryDelete,
 }
 
 #[derive(Debug)]
@@ -47,30 +81,31 @@ pub struct List<K, V> {
 }
 
 impl<K, V> List<K, V> {
-    // TODO: persistent
-    fn find_harris<const REC: bool>(
+    fn harris<const REC: bool>(
         &self,
         key: K,
-        value: V,
-        find: &mut Find<K, V>,
+        harris: &mut Harris<K, V>,
         tid: usize,
         guard: &Guard,
         pool: &PoolHandle,
-    ) -> Result<(bool, PShared<Node<K, V>>, PShared<Node<K, V>>), ()> {
-        let mut prev_next = self.curr;
+    ) -> Result<(bool, &DetectableCASAtomic<Node<K, V>>, PShared<Node<K, V>>), ()> {
+        let mut prev = &self.head;
+        let mut curr = self.head.load(Ordering::SeqCst, guard, pool);
+        let mut prev_next = curr;
+
         let found = loop {
-            let curr_node = some_or!(unsafe { self.curr.as_ref() }, break false);
-            let next = curr_node.next.load(Ordering::Acquire, guard);
+            let curr_node = some_or!(unsafe { curr.as_ref() }, break false);
+            let next = curr_node.next.load(Ordering::Acquire, guard, pool);
 
             if next.tag() != 0 {
-                self.curr = next.with_tag(0);
+                curr = next.with_tag(0);
                 continue;
             }
 
             match curr_node.key.cmp(key) {
                 Less => {
-                    self.curr = next.with_tag(0);
-                    self.prev = &curr_node.next;
+                    curr = next.with_tag(0);
+                    prev = &curr_node.next;
                     prev_next = next;
                 }
                 Equal => break true,
@@ -78,33 +113,49 @@ impl<K, V> List<K, V> {
             }
         };
 
+        let (found, prev, curr, prev_next) = ok_or!(
+            harris.result.checkpoint::<REC>(
+                (
+                    found,
+                    unsafe { prev.as_pptr(pool) },
+                    PAtomic::from(curr),
+                    PAtomic::from(prev_next)
+                ),
+                tid,
+                pool
+            ),
+            e,
+            (
+                e.0,
+                unsafe { e.1.deref(pool) },
+                e.2.load(Ordering::Relaxed, guard),
+                e.3.load(Ordering::Relaxed, guard)
+            )
+        );
         // If prev and curr WERE adjacent, no need to clean up
-        if prev_next == self.curr {
-            return Ok(found);
+        if prev_next == curr {
+            return Ok(found, prev, curr);
         }
 
         // cleanup marked nodes between prev and curr
-        self.prev
-            .compare_exchange(
-                prev_next,
-                self.curr,
-                Ordering::Release,
-                Ordering::Relaxed,
-                guard,
-            )
+        prev.cas::<REC>(prev_next, curr, &mut harris.help, tid, guard, pool)
             .map_err(|_| ())?;
 
         // defer_destroy from cursor.prev.load() to cursor.curr (exclusive)
         let mut node = prev_next;
-        while node.with_tag(0) != self.curr {
+        while node.with_tag(0) != curr {
             unsafe {
-                let next = node.as_ref().unwrap().next.load(Ordering::Acquire, guard);
-                guard.defer_destroy(node);
+                let next = node
+                    .as_ref()
+                    .unwrap()
+                    .next
+                    .load(Ordering::Acquire, guard, pool);
+                guard.defer_pdestroy(node);
                 node = next;
             }
         }
 
-        Ok(found)
+        Ok(found, prev, curr)
     }
 
     fn find<const REC: bool>(
@@ -114,13 +165,14 @@ impl<K, V> List<K, V> {
         tid: usize,
         guard: &Guard,
         pool: &PoolHandle,
-    ) -> (bool, PShared<Node<K, V>>, PShared<Node<K, V>>) {
-        if let Ok(r, prev, curr) = find::<REC>(&mut cursor, key, guard) {
+    ) -> (bool, &DetectableCASAtomic<Node<K, V>>, PShared<Node<K, V>>) {
+        if let Ok(r, prev, curr) = self.harris::<REC>(key, &mut find.harris, tid, guard, pool) {
             return (r, prev, curr);
         }
 
         loop {
-            if let Ok(r, prev, curr) = find::<false>(&mut cursor, key, guard) {
+            if let Ok(r, prev, curr) = self.harris::<false>(key, &mut find.harris, tid, guard, pool)
+            {
                 return (r, prev, curr);
             }
         }
@@ -145,7 +197,7 @@ impl<K, V> List<K, V> {
     pub fn try_insert<const REC: bool>(
         &self,
         node: PShared<Node<K, V>>,
-        prev: PShared<Node<K, V>>,
+        prev: &DetectableCASAtomic<Node<K, V>>,
         next: PShared<Node<K, V>>,
         try_ins: &mut TryInsert<K, V>,
         tid: usize,
@@ -155,11 +207,7 @@ impl<K, V> List<K, V> {
         node.next.store(next, Ordering::Relaxed);
         persist_obj(unsafe { &node.deref(pool).next }, true);
 
-        let prev_ref = unsafe { prev.deref(pool) };
-
-        prev_ref
-            .next
-            .cas::<REC>(next, node, &mut try_ins.delete, tid, guard, pool)
+        prev.cas::<REC>(next, node, &mut try_ins.insert, tid, guard, pool)
             .map(|_| ())
             .map_err(|_| ())
     }
@@ -224,7 +272,7 @@ impl<K, V> List<K, V> {
         &self,
         prev: PShared<Node<K, V>>,
         curr: PShared<Node<K, V>>,
-        try_del: &mut TryDelete<K, V>,
+        try_del: &mut TryDelete,
         tid: usize,
         guard: &Guard,
         pool: &PoolHandle,
@@ -240,7 +288,7 @@ impl<K, V> List<K, V> {
         if let Err(e) = curr_ref.next.cas::<REC>(
             next,
             next.with_tag(1),
-            &mut try_ins.delete,
+            &mut try_del.logical,
             tid,
             guard,
             pool,
@@ -254,7 +302,7 @@ impl<K, V> List<K, V> {
         while let Err(e) = curr_ref.next.cas::<false>(
             next,
             next.with_tag(1),
-            &mut try_ins.delete,
+            &mut try_del.logical,
             tid,
             guard,
             pool,
@@ -265,10 +313,8 @@ impl<K, V> List<K, V> {
             next = e;
         }
 
-        let prev_ref = unsafe { prev.deref() };
-        if prev_ref
-            .next
-            .cas::<REC>(next, node, &mut try_ins.delete, tid, guard, pool)
+        if prev
+            .cas::<REC>(next, node, &mut try_del.physical, tid, guard, pool)
             .is_ok()
         {
             unsafe { guard.defer_pdestroy(curr) };
