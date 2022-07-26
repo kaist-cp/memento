f45a79ea43f951af939ffebf9b0dbfe967900caa
Seungmin Jeon <seungmin.jeon@cp.kaist.ac.kr>
2022-07-26 07:58:17 +0000

    tcrash: Panic if dereference null pointer

--- src/pmem/ptr.rs
+++ src/pmem/ptr.rs
@@ -67,7 +67,7 @@ impl<T> PPtr<T> {
     /// When multiple pools are opened at the same time, the ptr of pool1 should not use the starting address of pool2.
     pub unsafe fn deref(self, pool: &PoolHandle) -> &'_ T {
         let addr = pool.start() + self.offset;
-        debug_assert!(pool.valid(addr));
+        debug_assert!(self != PPtr::null() && pool.valid(addr));
 
         &*(addr as *const T)
     }
@@ -80,7 +80,7 @@ impl<T> PPtr<T> {
     #[allow(clippy::mut_from_ref)]
     pub unsafe fn deref_mut(self, pool: &PoolHandle) -> &'_ mut T {
         let addr = pool.start() + self.offset;
-        debug_assert!(pool.valid(addr));
+        debug_assert!(self != PPtr::null() && pool.valid(addr));
         &mut *(addr as *mut T)
     }
 }
