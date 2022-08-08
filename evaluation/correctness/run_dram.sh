61054918f0ea20794ac838fba9a8967c5a5dd712
Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>
2022-08-08 21:44:59 +0900

    tcrash: Use timeout command
    Signed-off-by: Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>

--- evaluation/correctness/run_dram.sh
+++ evaluation/correctness/run_dram.sh
@@ -6,10 +6,11 @@ SCRIPT_DIR=`dirname $(realpath "$0")`
 PMEM_PATH="$SCRIPT_DIR/../../" # memento crate path
 COMMIT=$(git log -1 --format="%h")
 target=$1
-CNT_BUGS=30     # Number of saving bugs
+BUG_LIMIT=30     # Limitation of the number of saving pool file when a bug occurs
+TIMEOUT=10
 
 # Initialize
-nr_bug=0
+bug_cnt=0
 OUT_PATH="$SCRIPT_DIR/out_${COMMIT}/${target}"
 mkdir -p $PMEM_PATH/test
 mkdir -p $OUT_PATH
@@ -31,12 +32,12 @@ function dmsg() {
     echo "[$time] $msg" >> $log_tmp
 }
 
-function run_bg() {
+function run() {
     target=$1
     dmsg "run $target"
 
     rm -rf $PMEM_PATH/test/$target/*
-    RUST_BACKTRACE=1 RUST_MIN_STACK=10737418200 $SCRIPT_DIR/../../target/x86_64-unknown-linux-gnu/release/deps/memento-* $target::test --nocapture &>> $log_tmp &
+    RUST_BACKTRACE=1 RUST_MIN_STACK=10737418200 timeout $TIMEOUT $SCRIPT_DIR/../../target/x86_64-unknown-linux-gnu/release/deps/memento-* $target::test --nocapture &>> $log_tmp
 }
 
 # Test thread crash and recovery run.
@@ -46,38 +47,23 @@ while true; do
     log_tmp="$(mktemp)"
     dmsg "⎾⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺ thread crash-recovery test $target $i ⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⏋"
     start=$(date +%s%N)
-    run_bg $target
-    pid_bg=$!
-
-    limit=$((10 * 10**9))
-    while ps | grep $pid_bg > /dev/null; do
-        current=$(date +%s%N)
-        elapsed=$(($current-$start))
-        if [ $elapsed -gt $limit ]; then
-            kill -9 $pid_bg || true
-            dmsg "kill $pid_bg because it has been running for over 10 seconds."
-            break
-        fi
-    done
-
-    wait $pid_bg
+    run $target
     ext=$?
     if [ $ext -eq 0 ]; then
         pmsg "[${i}th test] success"
     else
         dmsg "fails with exit code $ext"
         pmsg "[${i}th test] fails with exit code $ext"
-        kill -9 $pid_bg || true
 
         # Save bug pool and logs
-        out_bug_path=$OUT_PATH/bug${nr_bug}_exit${ext}
+        out_bug_path=$OUT_PATH/bug${bug_cnt}_exit${ext}
         mkdir -p $out_bug_path
         cp -r $PMEM_PATH/test/$target/*.pool* $out_bug_path
         cp $log_tmp $out_bug_path/info.txt
 
         # Update output path of bug
-        nr_bug=$(($nr_bug+1))
-        if [ $nr_bug -eq $CNT_BUGS ]; then
+        bug_cnt=$(($bug_cnt+1))
+        if [ $bug_cnt -eq $BUG_LIMIT ]; then
             exit
         fi
     fi
