3c82de467c7de13288d3adc35d5860988e17cd2f
Seungmin Jeon <seungmin.jeon@cp.kaist.ac.kr>
2022-09-15 08:54:12 +0000

    Remove ssmem

--- src/pmem/mod.rs
+++ src/pmem/mod.rs
@@ -5,11 +5,9 @@ pub mod ll;
 pub mod pool;
 pub mod ptr;
 pub mod ralloc;
-pub mod ssmem;
 
 pub use global::*;
 pub use ll::*;
 pub use pool::*;
 pub use ptr::*;
 pub use ralloc::*;
-pub use ssmem::*;
