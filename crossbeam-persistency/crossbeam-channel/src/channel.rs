7195873f4669699933a64f44a7fe1ba0e35a94b3
Seungmin Jeon <seungmin.jeon@cp.kaist.ac.kr>
2022-09-22 09:37:17 +0000

    Add tests for channel

--- crossbeam-persistency/crossbeam-channel/src/channel.rs
+++ crossbeam-persistency/crossbeam-channel/src/channel.rs
@@ -6,7 +6,7 @@ use crossbeam_utils::Backoff;
 
 pub fn bounded<T>(cap: usize) -> (Sender<T>, Receiver<T>) {
     let queue = Box::into_raw(Box::new(ArrayQueue::<T>::new(cap)));
-    let counter = Box::into_raw(Box::new(Counter::default()));
+    let counter = Box::into_raw(Box::new(Counter::new()));
 
     (
         Sender {
@@ -24,6 +24,9 @@ pub struct Sender<T> {
     msgs: *mut ArrayQueue<T>,
     counter: *mut Counter,
 }
+
+unsafe impl<T> Send for Sender<T> {}
+unsafe impl<T> Sync for Sender<T> {}
 
 impl<T> Sender<T> {
     unsafe fn msgs(&self) -> &ArrayQueue<T> {
@@ -124,20 +127,26 @@ impl<T> Drop for Receiver<T> {
 #[derive(Default)]
 struct Counter {
     // left: number of sender
-    // right: number of receiver (n<=1 in this implementation because it is mpsc)
-    sender_recver: Atomic<(u32, u32)>,
+    // right: number of receiver (always n<=1 in this implementation because it is mpsc)
+    sender_recver: Atomic<(i32, i32)>,
 }
 
 impl Counter {
-    fn peek(&self) -> (u32, u32) {
+    fn new() -> Self {
+        Counter {
+            sender_recver: Atomic::new((1, 1)),
+        }
+    }
+
+    fn peek(&self) -> (i32, i32) {
         self.sender_recver.load(Ordering::SeqCst)
     }
 
-    fn add(&self, left: i32, right: i32) -> (u32, u32) {
+    fn add(&self, left: i32, right: i32) -> (i32, i32) {
         let backoff = Backoff::new();
         loop {
             let old = self.sender_recver.load(Ordering::SeqCst); // (cnt_sender, cnt_recver)
-            let new = (old.0 + left as u32, old.1 + right as u32); // (cnt_sender + left, cnt_recver + right);
+            let new = (old.0 + left, old.1 + right); // (cnt_sender + left, cnt_recver + right);
 
             if self
                 .sender_recver
@@ -149,5 +158,97 @@ impl Counter {
 
             backoff.snooze();
         }
+    }
+}
+
+/// Tests from https://doc.rust-lang.org/std/sync/mpsc/
+#[cfg(test)]
+mod test {
+    use crate::bounded;
+
+    /// Simple usage
+    #[test]
+    fn simple() {
+        use std::thread;
+
+        // Create a simple streaming channel
+        let (tx, rx) = bounded(1024);
+        thread::spawn(move || {
+            tx.send(10).unwrap();
+        });
+        assert_eq!(rx.recv().unwrap(), 10);
+    }
+
+    /// Shared usage
+    #[test]
+    fn shared() {
+        use std::thread;
+
+        // Create a shared channel that can be sent along from many threads
+        // where tx is the sending half (tx for transmission), and rx is the receiving
+        // half (rx for receiving).
+        let (tx, rx) = bounded(1024);
+        for i in 0..10 {
+            let tx = tx.clone();
+            thread::spawn(move || {
+                tx.send(i).unwrap();
+            });
+        }
+
+        for _ in 0..10 {
+            let j = rx.recv().unwrap();
+            assert!(0 <= j && j < 10);
+        }
+    }
+
+    /// Propagating panics
+    #[test]
+    fn prop_panic() {
+        // The call to recv() will return an error because the channel has already
+        // hung up (or been deallocated)
+        let (tx, rx) = bounded::<i32>(1024);
+        drop(tx);
+        assert!(rx.recv().is_err());
+    }
+
+    /// Synchronous channels
+    #[test]
+    fn sync_chan() {
+        use std::thread;
+
+        let (tx, rx) = bounded(1024);
+        thread::spawn(move || {
+            // This will wait for the parent thread to start receiving
+            tx.send(53).unwrap();
+        });
+        rx.recv().unwrap();
+    }
+
+    /// Unbounded receive loop
+    #[test]
+    fn unbounded_recv_loop() {
+        use std::thread;
+
+        let (tx, rx) = bounded(3);
+
+        for _ in 0..3 {
+            // It would be the same without thread and clone here
+            // since there will still be one `tx` left.
+            let tx = tx.clone();
+            // cloned tx dropped within thread
+            thread::spawn(move || tx.send("ok").unwrap());
+        }
+
+        // Drop the last sender to stop `rx` waiting for message.
+        // The program will not complete if we comment this out.
+        // **All** `tx` needs to be dropped for `rx` to have `Err`.
+        drop(tx);
+
+        // Unbounded receiver waiting for all senders to complete.
+        while let Ok(msg) = rx.recv() {
+            println!("{msg}");
+        }
+
+        println!("completed");
     }
 }
